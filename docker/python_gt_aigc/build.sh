37939a75ade0f3a5e728c4cf4ead17714ebf90bb
李露 <lilu1@hydsoft.com>
2023-11-17 14:48:22 +0800

    镜像操作层级优化

--- docker/python_gt_aigc/build.sh
+++ docker/python_gt_aigc/build.sh
@@ -6,6 +6,8 @@ docker login 10.152.160.83:60919
 latest_ver=3.10-slim-bullseye-gt-aigc-$(date +"%Y%m%d%H%M%S")
 images_name=10.152.160.83:60919/gt-base/python-aigc
 
+cp ../../requirements.txt ./
+
 echo "images info:$images_name"
 
 echo "build images..."
@@ -20,3 +22,5 @@ echo "images push..."
 docker push $images_name:"$latest_ver"
 
 docker logout
+
+rm -f requirements.txt
